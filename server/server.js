f15edf523acf33d44b428640d7aa303121019571
fredericbouin <bouinfrederic@gmail.com>
2025-08-27 15:21:53 -0400

    funky gfuckldasd still finked

--- server/server.js
+++ server/server.js
@@ -9,10 +9,19 @@ const port = process.env.PORT || 3001;
 
 // --- DYNAMIC CORS CONFIGURATION ---
 // Use an environment variable for the client URL
-const clientURL = process.env.CLIENT_URL; 
+
+const clientURL = process.env.CLIENT_URL;
+
+const whitelist = [clientURL, 'http://localhost:3000']; // Add your local dev URL if needed
 
 const corsOptions = {
-  origin: clientURL, // Use the variable here
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   optionsSuccessStatus: 200
